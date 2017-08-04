675db7b46733339ce19c71019cdcf3f0657758b6
Joshua Simmons <joshua.simmons@emptypath.com>
2017-08-04 14:41:48 -0700

    implement connected components and to_assoc_list functions

--- score.py
+++ score.py
@@ -11,3 +11,35 @@ def score_game(mines, rivers, players):
   '''
   # TODO: implement
   return [0 for _ in players]
+
+def to_assoc_list(rivers):
+  res = {}
+  def add_one(a, b):
+    res.setdefault(a, set()).add(b)
+  for x, y in rivers:
+    add_one(x, y)
+    add_one(y, x)
+  return res
+
+def connected_components(rivers_m):
+  def go(start):
+    seen, todo = set(), [start]
+    while todo:
+      v = todo.pop()
+      if v not in seen:
+        seen.add(v)
+        try:
+          for c in rivers_m[v]:
+            if c not in seen:
+              todo.append(c)
+        except KeyError:
+          pass
+    return seen
+  vs = set(rivers_m.keys())
+  acc = []
+  while vs:
+    res = go(vs.pop())
+    for v in res:
+      vs.discard(v)
+    acc.append(res)
+  return acc
