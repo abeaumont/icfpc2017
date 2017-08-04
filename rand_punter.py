b492d93e83c1aea5c27e4b59760c8c6a424ade49
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2017-08-04 22:17:23 +0200

    Allow to save a game from client

--- rand_punter.py
+++ rand_punter.py
@@ -1,12 +1,18 @@
+import json
+import os
 import interface
+import uuid
 
 class RandPunter(interface.Punter):
-    def __init__(self, name, init_state):
+    def __init__(self, name, init_state, fname=None):
         super(RandPunter, self).__init__(name, init_state)
+        self.fname = fname if fname is not None else str(uuid.uuid4())
+        self.all_turns = []
         self.available_rivers = {(r['source'], r['target']) for r in self.rivers}
         
     def turn(self, state):
         self.log("state: %s", state)
+        self.all_turns.extend(state['move']['moves'])
         for m in state['move']['moves']:
             if 'claim' in m:
                 s = m['claim']['source']
@@ -17,6 +23,22 @@ class RandPunter(interface.Punter):
             return self.pass_()
         else:
             return self.claim(*self.available_rivers.pop())
+
+    def stop(self, state):
+        try:
+            os.makedirs("output")
+        except OSError:
+            pass
+
+        json_obj = {
+            "turns" : self.all_turns,
+            "num_players" : self.punters
+        }
+        json_str = json.dumps(json_obj)
+        fname = os.path.join('output', self.fname)
+        with open(fname, "w") as f:
+            f.write(json_str)
+        print "SAVED GAME TO", fname, "SIZE IS", len(json_str)
         
 
 if __name__ == '__main__':
