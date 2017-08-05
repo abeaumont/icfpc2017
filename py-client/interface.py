6ce6741182a2e60407f9dcbbd7bdfc188d3e5396
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2017-08-05 13:07:01 +0200

    fname fix and more work on offline mode

--- py-client/interface.py
+++ py-client/interface.py
@@ -7,7 +7,7 @@ import uuid
 from helpers import game_url
 
 class Punter(object):
-    def __init__(self, name, state, fname):
+    def __init__(self, name, state, fname=None):
         self.fname = fname if fname is not None else str(uuid.uuid4())
         self.state = state
         self.name = name
@@ -47,6 +47,7 @@ class Punter(object):
         state['available_rivers'] = self.available_rivers
         state['all_turns'] = self.all_turns
         state['neighbors'] = self.neighbors
+        return state
 
     def turn(self, state):
         """Your logic per turn goes here"""
@@ -60,10 +61,6 @@ class Punter(object):
 
     def pass_(self):
         return {'pass': {'punter': self.punter}}
-
-    def log(self, message, *args):
-        print >>sys.stderr, "[%s] %s" % (self.name, (message % map(str, args)))
-
 
     def upkeep_punter(self, state):
         if 'move' in state:
@@ -95,9 +92,9 @@ class Punter(object):
         fname = os.path.join('output', self.fname + '.json')
         with open(fname, "w") as f:
             f.write(json_str)
-        print "SAVED GAME TO", game_url(fname), "SIZE IS", len(json_str)
-        
-        
+        self.log("SAVED GAME TO %s SIZE IS %d", game_url(fname), len(json_str))
+
+
 class Interface(object):
     def __init__(self, name, punter_class, server):
         self.name = name
@@ -108,22 +105,25 @@ class Interface(object):
     def _send(self, msg):
         msg = json.dumps(msg)
         msg = '{}:{}'.format(len(msg), msg)
-        print('sending message: ', msg)
+        self.log('sending message: %s', msg)
         self.server.write(msg)
         self.server.flush()
 
     def _recv(self):
         line = self.server.readline()
-        print('receiving message: ', line)
+        self.log('receiving message: %s', line)
         if not line:
             raise EOFError()
         return json.loads(line.split(':', 1)[1])
+
+    def log(self, message, *args):
+        print >>sys.stderr, "[%s] %s" % (self.name, (message % map(str, args)))
 
     def run(self):
         self._send({'me': self.name})
         self._recv()
         init = self._recv()
-        print init
+        self.log(init)
         self._send({'ready': init['punter']})
         self.punter = self.punter_class(self.name, init)
         while True:
@@ -138,23 +138,51 @@ class Interface(object):
             turn = self.punter.turn(state)
             self._send(turn)
 
-
 class OfflineInterface(object):
     def __init__(self, name, punter_class):
         self.name = name
         self.punter_class = punter_class
         self.punter = None
+        self.fd = open('offline' + str(uuid.uuid4()) + '.log', 'w')
+
+    def _send(self, msg):
+        msg = json.dumps(msg)
+        msg = '{}:{}'.format(len(msg), msg)
+        self.log('sending message: %s', msg)
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+
+    def _recv(self):
+        s = ''
+        c = sys.stdin.read(1)
+        while c != ':':
+            s += c
+            c = sys.stdin.read(1)
+        line = sys.stdin.read(int(s))
+        self.log('receiving message: %s', line)
+        if not line:
+            raise EOFError()
+        try:
+            return json.loads(line)
+        except e:
+            self.log('error: %s', str(e))
+
+    def log(self, message, *args):
+        print >>self.fd, "[%s] %s" % (self.name, (message % map(str, args)))
 
     def run(self):
         self._send({'me': self.name})
-        self._recv()
+        msg = self._recv()
         msg = self._recv()
         if 'punter' in msg:
-            self.punter = self.punter_class(self.name, msg)
-            self._send({
-                'ready': init['punter'],
-                'state': self.punter.get_state()
-            })
+            try:
+                self.punter = self.punter_class(self.name, msg)
+                self._send({
+                    'ready': msg['punter'],
+                    'state': self.punter.get_state()
+                })
+            except Exception as e:
+                self.log('error: %s', str(e))
         elif 'move' in msg:
             self.punter = self.punter_class(self.name, msg['state'])
             self.punter.upkeep_punter(msg)
@@ -165,3 +193,4 @@ class OfflineInterface(object):
             self.punter = self.punter_class(self.name, msg['state'])
             self.punter.upkeep_punter(msg)
             self.punter.stop(msg)
+        self.fd.close()
