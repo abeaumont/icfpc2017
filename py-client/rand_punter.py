1cae1739ff5fe3e606cdb96ab0a29bb57bf500f4
Alfredo Beaumont <alfredo.beaumont@gmail.com>
2017-08-05 11:57:47 +0200

    Add some randomness to random punter

--- py-client/rand_punter.py
+++ py-client/rand_punter.py
@@ -1,6 +1,7 @@
 import json
 import os
 import interface
+import random
 import uuid
 
 class RandPunter(interface.Punter):
@@ -8,12 +9,16 @@ class RandPunter(interface.Punter):
         super(RandPunter, self).__init__(name, init_state)
         self.fname = fname if fname is not None else str(uuid.uuid4())
         self.all_turns = []
+        random.seed()
         
     def turn(self, state):
         self.log("state: %s", state)
         if len(self.available_rivers) == 0:
             return self.pass_()
         else:
+            r = random.randint(0, len(self.available_rivers) - 1)
+            for i in range(r):
+                self.available_rivers.add(self.available_rivers.pop())
             return self.claim(*self.available_rivers.pop())
 
 if __name__ == '__main__':
