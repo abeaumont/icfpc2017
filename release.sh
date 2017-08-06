03cff36a5987d330abec2aad7f8dbd70802d0ca6
exit70 <exit70@mail.wtf>
2017-08-05 18:30:12 -0700

    release mst punter

---
+++ release.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+CODE=924bdb4c-8d8d-4705-a32d-75c42e574c29
+SRC=py-client
+DEST=icfp-$CODE
+
+cp $SRC/*.py $DEST/
+cp $SRC/*.py $DEST/src
+tar -C $DEST/ -czvf $DEST.tar.gz --exclude='*.pyc' .
+rm $DEST/*.py
+rm $DEST/src/*.py
