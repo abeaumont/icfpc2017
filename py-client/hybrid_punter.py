d6a457f4478c6190c9a7184ca86dfb279856acf8
okay <okay@chalk>
2017-08-06 18:08:03 -0700

    [hybrid] update greedy threshold for hybrid hunter

--- py-client/hybrid_punter.py
+++ py-client/hybrid_punter.py
@@ -9,7 +9,7 @@ def HybridPunter(name, init_state):
 
     density = float(len(rivers)) / float(len(sites))
 
-    if density < 1.75 and len(sites) < 200:
+    if density < 2 and len(sites) < 200:
         return greedy_punter.GreedyPunter(name, init_state)
     else:
         return dfs_punter.DFSPunter(name, init_state)
